fe02a5fe375cd9b59791251b1a61c0d4495842c7
James B <audsecondarylap@outlook.com>
2022-11-20 11:50:05 -0500

    font assign

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../sass/layout/_Header.scss';
 
 const Header = () => {
   return (
     <header className="Header">
       <h1 className="Header-title">Where in the world?</h1>
-      <div className="Header-theme-switcher"></div>
+      <div className="Header-theme-toggle"></div>
     </header>
   );
 };
