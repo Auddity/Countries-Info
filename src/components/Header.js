0a47b9e6b6e62a85c1a29d6d7ec72db5c6de5537
James B <audsecondarylap@outlook.com>
2022-11-20 11:58:59 -0500

    install react-icons, header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../sass/layout/_Header.scss';
+import { BsMoon, BsMoonFill } from 'react-icons/bs';
 
 const Header = () => {
   return (
     <header className="Header">
       <h1 className="Header-title">Where in the world?</h1>
-      <div className="Header-theme-toggle"></div>
+      <div className="Header-theme-toggle">
+        <div className="icon-container">
+          <BsMoon />
+        </div>
+        <p>Dark Mode</p>
+      </div>
     </header>
   );
 };
