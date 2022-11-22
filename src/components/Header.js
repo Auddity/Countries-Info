2092233c45b42635c6d8c8ee8cdd8ae78d1b2dfb
James B <audsecondarylap@outlook.com>
2022-11-22 14:09:25 -0500

    header dark theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,15 @@ const Header = () => {
   const dispatch = useDispatch();
   const { isDark } = useSelector(store => store.theme);
 
-  console.log(isDark);
-
   return (
-    <header className="Header">
+    <header className={!isDark ? 'Header' : 'Header dark'}>
       <h1 className="Header-title">Where in the world?</h1>
       <div
         className="Header-theme-toggle"
         onClick={() => dispatch(changeTheme())}
       >
         <div className="icon-container">
-          {!isDark ? <BsMoon /> : <BsMoonFill style={{ color: 'white' }} />}
+          {!isDark ? <BsMoon /> : <BsMoonFill />}
         </div>
         <p>Dark Mode</p>
       </div>
